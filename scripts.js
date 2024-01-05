e9041a37c77b97a0869ffa1c2927295add00c17f
buriedsignals <tom.vaillant@icloud.com>
2024-01-05 10:20:11 +0100

    finessing barba js initialization

--- scripts.js
+++ scripts.js
@@ -1,36 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Barba transitions
-    barba.init({
-        transitions: [{
-            name: 'opacity-transition',
-            leave(data) {
-            // Animate the opacity of the current container to 0
-            return gsap.to(data.current.container, {
-                opacity: 0,
-                duration: 1
-            });
-            },
-            enter(data) {
-            // Start the next container at opacity 0
-            gsap.from(data.next.container, {
-                opacity: 0
-            });
-            // Animate the opacity of the next container to 1
-            return gsap.to(data.next.container, {
-                opacity: 1,
-                duration: 1
-            });
-            }
-        }]
-    });
-    // Constants
+function storyPageScripts() {
     const triggerElement = document.getElementById('scrolly-text-3');
     const currentImage = document.querySelector('.frame-image'); 
     const image2URL = 'https://uploads-ssl.webflow.com/655a5e3e34bc8a89769ff74e/6583f8350811f7701cc61e05_space.webp';
 
     let animationStarted = false; // Flag to ensure the animation only starts once
 
-    // Function to check if the element is at the top 30% of the viewport
     function isInViewport() {
         const rect = triggerElement.getBoundingClientRect();
         return (
@@ -41,19 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function imageTransition(newImageUrl) {
         currentImage.srcset = newImageUrl;
-        console.log('animation triggered')
     }
 
     window.addEventListener('scroll', () => {
         if (isInViewport() && !animationStarted) {
-            console.log('Scroll event triggered');
             imageTransition(image2URL);
             animationStarted = true;
         }
     });
+}
+
+// For initial page load
+document.addEventListener('DOMContentLoaded', storyPageScripts);
+
+// Define initScripts function for Barba.js
+function initScripts() {
+    storyPageScripts(); // Re-setup the page scripts
+}
+
+// Barba.js initialization
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave(data) {
+            return gsap.to(data.current.container, { opacity: 0, duration: 1 });
+        },
+        enter(data) {
+            initScripts();
+            gsap.from(data.next.container, { opacity: 0 });
+            return gsap.to(data.next.container, { opacity: 1, duration: 1 });
+        }
+    }]
 });
-
-
-    // https://uploads-ssl.webflow.com/655a5e3e34bc8a89769ff74e/655a5f21aced22f21d962899_COCKPIT.webp
-    // https://uploads-ssl.webflow.com/655a5e3e34bc8a89769ff74e/6583f8350811f7701cc61e05_space.webp
-    // https://uploads-ssl.webflow.com/655a5e3e34bc8a89769ff74e/6584470007d534b8dad74572_nebula.webp
